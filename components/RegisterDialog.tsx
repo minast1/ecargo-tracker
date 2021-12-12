a1b9609b3dd42de3f21265de2ad8d67983320df7
Edmond Marfo <edmarfo2@gmail.com>
2021-12-12 01:23:20 +0000

    registerDialog component

---
+++ components/RegisterDialog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterDialog = () => {
+    return (
+        <div>
+            I am the register dialog component
+        </div>
+    )
+}
+
+export default RegisterDialog
