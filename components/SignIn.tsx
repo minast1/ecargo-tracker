aebd745d9199c462d3355c9c3ced90b901443ec3
Edmond Marfo <edmarfo2@gmail.com>
2021-12-22 20:39:37 +0000

    change in Signin

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -34,7 +34,7 @@ export default function SignIn() {
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
      
-    data.callbackUrl =  `${process.env.NEXTAUTH_PULIC_URL}/admin-area/home`
+    data.callbackUrl =  `${process.env.NEXTAUTH_PULIC_URL}/home`
    // data.redirect = false;
     authStore.setState({ loading: true });
     signIn('credentials' , data  );
